842350a23e13fdd4c62dd281eefddf64db3e193c
brajasekharreddy <raj479.mits@gmail.com>
2023-02-08 23:25:56 +0530

    terraform modules

---
+++ 06-modules/modules.tf
@@ -0,0 +1,24 @@
+//Module 1
+module "sg" {
+  source = "./sg"
+}
+
+//Module 2
+module "ec2" {
+  source            = "./ec2"
+  //send the output of sg module to ec2 module
+  security_group_id = module.sg.security_group_id
+}
+
+terraform {
+  backend "s3" {
+    bucket = "rajstatefilebucket"
+    key    = "05-remote-state/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
