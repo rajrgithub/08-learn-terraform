0ab1bf26c49864b22bf5fdaa10f973c27a75283b
brajasekharreddy <raj479.mits@gmail.com>
2023-02-08 23:55:33 +0530

    terraform functions

---
+++ 07-functions/main.tf
@@ -0,0 +1,21 @@
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.centos8.id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "test-centos8"
+  }
+}
+
+data "aws_ami" "centos8" {
+  most_recent = true
+  name_regex  = "Centos-8-DevOps-Practice"
+  owners      = ["973714476881"]
+}
+
+output "instance_profile" {
+  // Below will fail
+  #value = aws_instance.web.*.id[1]
+  // to avoid failure use try
+  value = try(aws_instance.web.*.id[1], "")
+}
