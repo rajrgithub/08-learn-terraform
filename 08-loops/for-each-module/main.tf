90e82cb2a9204f99d6a27952fb79cc31f69176c8
brajasekharreddy <raj479.mits@gmail.com>
2023-02-09 22:56:03 +0530

    for each using Module - Best Approach

---
+++ 08-loops/for-each-module/main.tf
@@ -0,0 +1,32 @@
+variable "components" {
+  default = {
+    cart = {
+      name          = "cart",
+      instance_type = "t3.small"
+    }
+    catalogue = {
+      name          = "catalogue",
+      instance_type = "t3.micro"
+    }
+  }
+}
+
+module "ec2" {
+  source = "./module"
+
+  for_each      = var.components
+  instance_type = each.value.instance_type
+  name          = each.value.name
+}
+
+## Always iterate modules, not resources.
+## Always map the data and use for_each loop
+
+output "publicip" {
+  // value = module.ec2
+
+  value = {
+    for k, v in module.ec2 : k => v["ec2"].public_ip
+  }
+}
+
---
+++ 08-loops/for-each-module/module/main.tf
@@ -0,0 +1,24 @@
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.centos8.id
+  instance_type = var.instance_type
+
+  tags = {
+    Name = var.name
+  }
+}
+
+
+data "aws_ami" "centos8" {
+  most_recent = true
+  name_regex  = "Centos-8-DevOps-Practice"
+  owners      = ["973714476881"]
+}
+
+variable "instance_type" {
+  default = "t3.micro"
+}
+variable "name" {}
+
+output "ec2" {
+  value = aws_instance.web
+}
