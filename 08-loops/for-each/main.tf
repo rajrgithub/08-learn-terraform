5d1db1590aeabb9ccb4cd6b45c4ba26bdf82f421
brajasekharreddy <raj479.mits@gmail.com>
2023-02-09 22:29:36 +0530

    for-each to handle map

---
+++ 08-loops/for-each/main.tf
@@ -0,0 +1,39 @@
+resource "aws_instance" "web" {
+  for_each      = var.components
+  ami           = data.aws_ami.centos8.id
+  instance_type = each.value.instance_type
+
+  tags = {
+    Name = each.value["name"]
+  }
+}
+
+
+data "aws_ami" "centos8" {
+  most_recent = true
+  name_regex  = "Centos-8-DevOps-Practice"
+  owners      = ["973714476881"]
+}
+
+
+output "publicip" {
+  #value = aws_instance.web
+
+  value = {
+    for k, v in aws_instance.web : k => v.public_ip
+  }
+}
+
+variable "components" {
+  default = {
+    cart = {
+      name          = "cart",
+      instance_type = "t3.small"
+    }
+    catalogue = {
+      name          = "catalogue",
+      instance_type = "t3.micro"
+    }
+  }
+}
+
