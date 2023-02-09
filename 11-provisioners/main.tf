c0cfe43f27c246e47bc7df1c6d3190f202fe2fba
brajasekharreddy <raj479.mits@gmail.com>
2023-02-10 00:51:37 +0530

    provisioner

---
+++ 11-provisioners/main.tf
@@ -0,0 +1,82 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+data "aws_ami" "centos8" {
+  most_recent = true
+  name_regex  = "Centos-8-DevOps-Practice"
+  owners      = ["973714476881"]
+}
+
+resource "aws_instance" "web" {
+  ami                    = data.aws_ami.centos8.id
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+
+  tags = {
+    Name        = "test-centos8"
+  }
+
+  /*
+
+ // Best Practice is to create provisioner outside resource
+ // Instance will be destroyed and recreated because of false command in provisioner
+ //to avoid instance recreation create provisioner with null_resource
+  provisioner "remote-exec" {
+    connection {
+      host     = self.public_ip
+      user     = "centos"
+      password = "DevOps321"
+    }
+
+  // Provide commands to execute
+    inline = [
+      "false"
+    ]
+  }
+
+  */
+
+}
+
+//Only provisioner will be recreated NOT the instance
+resource "null_resource" "provision" {
+
+  provisioner "remote-exec" {
+    connection {
+      host     = aws_instance.web.public_ip
+      user     = "centos"
+      password = "DevOps321"
+    }
+
+    inline = [
+      "false"
+      //"echo Hello"
+    ]
+  }
+}
+
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
