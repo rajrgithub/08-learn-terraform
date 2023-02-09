9b5b103b4854b2c681fadf28cb1f433f52172bd6
brajasekharreddy <raj479.mits@gmail.com>
2023-02-09 23:12:00 +0530

    conditions

---
+++ 09-conditions/main.tf
@@ -0,0 +1,26 @@
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.centos8.id
+  instance_type = var.type == "null" ? "t3.micro" : var.type
+
+  tags = {
+    Name = "demo"
+  }
+}
+
+
+data "aws_ami" "centos8" {
+  most_recent = true
+  name_regex  = "Centos-8-DevOps-Practice"
+  owners      = ["973714476881"]
+}
+
+variable "type" {
+  default = "null"
+}
+
+# terraform init
+
+# terraform plan
+
+# terraform plan -var type="t3.large"
+
